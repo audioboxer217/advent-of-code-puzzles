35bf380da53ebe40a9674411d25e134858836b81
scott.eppler <scott.eppler@coda.global>
2017-12-02 07:55:37 -0600

    Add my solution for part 1

---
+++ day2/python/solve_puzzle_1.py
@@ -0,0 +1,36 @@
+"""Calculates a Checksum from CSV"""
+
+import csv
+
+def get_values(row):
+    """Gets the largest and smallest values from a list of numbers"""
+    smallest = row[0]
+    largest = row[0]
+    for curr_num in row:
+        if int(curr_num) < int(smallest):
+            smallest = curr_num
+        if int(curr_num) > int(largest):
+            largest = curr_num
+    return smallest, largest
+
+def get_difference(smallest, largest):
+    """Determines the difference between two values"""
+    difference = int(largest) - int(smallest)
+    return difference
+
+
+def main():
+    """Calculates a Checksum from CSV"""
+    with open("../puzzle_input.csv") as file:
+        csv_reader = csv.reader(file, delimiter='\t')
+        num_list = []
+        for row in csv_reader:
+            smallest, largest = get_values(row)
+            difference = int(largest) - int(smallest)
+            num_list.append(int(difference))
+        checksum = sum(num_list)
+        print("Checksum: %s" % checksum)
+
+if __name__ == '__main__':
+    main()
+    
