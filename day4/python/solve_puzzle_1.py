a57228b5df64473dbca1c938abbd043e0dbeee90
scott.eppler <scott.eppler@coda.global>
2017-12-05 15:36:29 -0600

    Add solution to Day 4 Part 1

---
+++ day4/python/solve_puzzle_1.py
@@ -0,0 +1,27 @@
+''' Count Valid passphrases '''
+
+def validate_passphrs(passphrs):
+    ''' Validate Passphrase '''
+
+    words = passphrs.split()
+
+    for word in words:
+        word_count = words.count(word)
+        if word_count > 1:
+            return False
+    return True
+
+def main():
+    ''' Count Valid passphrases '''
+
+    with open("../input.txt") as file:
+        valid_count = 0
+        for passphrs in file:
+            if validate_passphrs(passphrs):
+                valid_count += 1
+
+        print("Valid Passwords: %s" % valid_count)
+
+if __name__ == '__main__':
+    main()
+    
