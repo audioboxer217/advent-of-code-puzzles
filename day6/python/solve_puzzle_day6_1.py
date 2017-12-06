f4ab39148218496dcc6c2d5429be1ccbb7f2c8e8
scott.eppler <scott.eppler@coda.global>
2017-12-06 11:11:46 -0600

    Add solution for Day 6 Part 1

---
+++ day6/python/solve_puzzle_day6_1.py
@@ -0,0 +1,45 @@
+''' Look for Infinite Loop in Memory Redistribution '''
+
+def get_largest_block(mem_banks):
+    ''' Get the largest memory block and it's index '''
+
+    sorted_banks = sorted(mem_banks, reverse=True)
+    largest = sorted_banks[0]
+    index = mem_banks.index(largest)
+
+    return index, largest
+
+def redist_blocks(mem_banks, index, blocks):
+    ''' Redistribute the memory banks '''
+
+    mem_banks[index] = 0
+    index += 1
+    mem_bank_size = len(mem_banks)
+
+    for block in range(blocks):
+        new_index = index + block
+        while new_index >= mem_bank_size:
+            new_index -= mem_bank_size
+        mem_banks[new_index] += 1
+
+    return mem_banks
+
+def main():
+    ''' Look for Infinite Loop in Memory Redistribution '''
+
+    file = open("../puzzle_input.txt")
+    mem_banks = [int(x) for x in file.read().split('\t')]
+    redistributions = []
+    redist_count = 0
+
+    while redistributions.count(str(mem_banks)) <= 1:
+        index, largest = get_largest_block(mem_banks)
+        mem_banks = redist_blocks(mem_banks, index, largest)
+        redistributions.append(str(mem_banks))
+        redist_count += 1
+
+    print("Redistribution Count: %s" % redist_count)
+
+if __name__ == '__main__':
+    main()
+    
