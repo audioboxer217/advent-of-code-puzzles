804244b5214b3b6f23bfe56de56f2f930f4ff97f
scott.eppler <scott.eppler@coda.global>
2017-12-13 07:53:39 -0600

    Add solution for Day 12 Part 2

---
+++ day12/python/solve_puzzle_day12_2.py
@@ -0,0 +1,38 @@
+''' Digital Plumber '''
+
+def associate_pipes(program, associations):
+    ''' Completes the path assocations for a program '''
+
+    i = 0
+
+    while i < len(program):
+        for item in associations:
+            if program[i] in item:
+                program = list(set(program + item))
+                associations.remove(item)
+                i = 0
+        i += 1
+
+    return program, associations
+
+def main():
+    ''' Digital Plumber '''
+
+    associations = []
+    with open("../puzzle_input.txt") as file:
+        for line in file:
+            associations.append(list(map(int, (line.split(" <-> "))[1].split(", "))))
+
+    for idx, item in enumerate(associations):
+        associations[idx] = (list(set(item+[idx])))
+
+    pipes = []
+    while len(associations) > 0:
+        program, associations = associate_pipes(associations[0], associations)
+        pipes.append(program)
+
+    print(len(pipes))
+
+if __name__ == '__main__':
+    main()
+    
