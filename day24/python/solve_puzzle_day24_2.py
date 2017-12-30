9fc8a9d6e5a5c26659149b172a66c2dc5a885614
scott.eppler <scott.eppler@coda.global>
2017-12-30 08:04:23 -0600

    Add solutions for Day 24 Part 2

---
+++ day24/python/solve_puzzle_day24_2.py
@@ -0,0 +1,48 @@
+''' Electromagnetic Moat '''
+
+import numpy as np
+
+def build_bridge(parts, port, weight, i):
+    ''' Build a bridge with possible solutions '''
+
+    bridge = []
+    i += 1
+    matches = np.where(np.array(parts) == port)[0].tolist()
+    if matches:
+        for index in matches:
+            curr_part = parts[index]
+            next_port = curr_part[curr_part.index(port)-1]
+            tmp_parts = []
+            for part in parts:
+                if part != curr_part:
+                    tmp_parts.append(part)
+            bridge.extend(build_bridge(tmp_parts, next_port, weight + sum(curr_part), i+1))
+        return bridge
+    else:
+        return [[weight, i]]
+
+def main():
+    ''' Electromagnetic Moat '''
+
+    parts = open('../puzzle_input.txt').read().split('\n')
+    for i, _ in enumerate(parts):
+        parts[i] = list(map(int, parts[i].split("/")))
+
+    bridges = build_bridge(parts, 0, 0, 0)
+
+    longest = 0
+    for attributes in bridges:
+        if attributes[1] > longest:
+            longest = attributes[1]
+
+    strongest = 0
+    for attributes in bridges:
+        if attributes[1] == longest:
+            if attributes[0] > strongest:
+                strongest = attributes[0]
+
+    print(strongest)
+
+if __name__ == '__main__':
+    main()
+    
